6952804fa31c45509059252a83fa6d434777ca2b
ADMIN-PC\Admin <quangnx@gmail.com>
2025-07-19 17:24:04 +0700

    First create a java 3.x consumer run for specific target

---
+++ src/main/java/com/persona/mileage/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.persona.mileage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
